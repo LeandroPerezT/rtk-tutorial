eac0528cc356b857c40bb0f0a384dc7ac6111fbe
Leandro Perez Torres <leandropereztorres98@gmail.com>
2021-10-08 20:03:11 -0300

    Fix import typo

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import counterReducer from './cards/counterSlice'
+import counterReducer from './card/counterSlice'
 
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
 import { pokemonApi } from "./services/pokeApi";
